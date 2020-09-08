df36fcaf1c4e70bbf56a5571426a86a0f7c85cff
Abdulaziz <abdulazizfaactory@gmail.com>
2020-09-09 00:04:48 +0500

    Adaptation of the tiny slider in Pulse

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,44 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        spped: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev slick-button"><img src = "icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next slick-button"><img src = "icons/right.svg"></button>',
-        responsive: [
-         {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-        }
-        ]
-    });
-});
+// Slick Slider
+// $(document).ready(function () {
+//     $('.carousel__inner').slick({
+//         spped: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev slick-button"><img src = "icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next slick-button"><img src = "icons/right.svg"></button>',
+//         responsive: [
+//          {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//         }
+//         ]
+//     });
+// });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    navPosition: "bottom",
+    controls: false,
+    autoHeight: true,
+    responsive: {
+      640: {
+        gutter: 20,
+        items: 1,
+       
+      },
+      700: {
+      },
+      900: {
+        items: 1,
+      }
+    }
+  });
+  
+  document.querySelector('.prev').addEventListener('click',function () {
+    slider.goTo('prev');});
+    document.querySelector('.next').addEventListener('click',function () {
+      slider.goTo('next');});
