39152bc9d7a89d02cc6422162e568cdb398eb85b
Abdulaziz <abdulazizfaactory@gmail.com>
2021-06-08 12:44:11 +0500

    understood how to reuse modificators, and modal html, css done from lesson

--- src/js/script.js
+++ src/js/script.js
@@ -16,58 +16,56 @@
 //         ]
 //     });
 // });
-// const slider = tns({
-//   container: '.carousel__inner',
-//   items: 1,
-//   slideBy: 'page',
-//   autoplay: false,
-//   navPosition: "bottom",
-//   controls: false,
-//   autoHeight: true,
-//   responsive: {
-//     600: {
-//       controls: false
-//     }
-//   }
-// });
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  navPosition: "bottom",
+  controls: false,
+  autoHeight: true,
+  responsive: {
+    600: {
+      controls: false
+    }
+  }
+});
 
-// document.querySelector('.prev').addEventListener('click', function () {
-//   slider.goTo('prev');
-// });
-// document.querySelector('.next').addEventListener('click', function () {
-//   slider.goTo('next');
-// });
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
 
-// $(document).ready(function () {
-//   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-//     $(this)
-//       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-//       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-//   });
+$(document).ready(function () {
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
  
  
-//       function toggleSlide(item) {
-//         $(item).each(function (i) {
-//           $(this).on('click', function(e) 
-//           {
-//             e.preventDefault();
-//             $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-//             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-//           });
-//             });
-//       }
-//       toggleSlide('.catalog-item__link');
-//       toggleSlide('.catalog-item__back');
+      function toggleSlide(item) {
+        $(item).each(function (i) {
+          $(this).on('click', function(e)
+          {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          });
+            });
+      }
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
       // Modal
 	const modalTrigger = document.querySelectorAll('[data-buy]'),
 		modal = document.querySelector('.modal_buy');
-
 		function openModal() {
 			modal.classList.add('show');
 			modal.classList.remove('hide');
 			clearInterval(modalTimerID); // if modal is opened already, we close it
 		}
-
 		function closeModal() {
 			modal.classList.add('hide');
 			modal.classList.remove('show');
@@ -90,7 +88,7 @@
 			}
 		});
 
-		const modalTimerID = setTimeout(openModal, 3000)
+		const modalTimerID = setTimeout(openModal, 3000);
+w
 
-
-// });
+});
